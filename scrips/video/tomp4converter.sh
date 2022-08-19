82c90009ba6cbed5d8d9e421ab118e078e4d5d7d
Rui Constancio <r.miguel.a.c@gmail.com>
2022-08-19 10:11:44 -0300

    Add mime check to mp4converter script

--- scrips/video/tomp4converter.sh
+++ scrips/video/tomp4converter.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/env bash
 
 list_videos() {
-	REC_VIDS=$(find ${RECS_FOLDER} -type f -name "*.mp4" -size +17M)
+	if [ -d $1 ]; then
+		REC_VIDS=$(find ${RECS_FOLDER} -type f -name "*.mp4" -size +17M)
+	else
+		REC_VIDS=$1	
+	fi
 }
 
 convert() {
 	for v in ${REC_VIDS}; do
 	    echo "[INFO] Converting ${v}"
-        mv ${v} ${v/mp4/mpeg}
-        # ADD A check to validate the mime
-	    ffmpeg -loglevel quiet -i ${v/mp4/mpeg} -c copy ${v}
-        if [ $? -eq 0 ]; then
-            echo "[INFO] conversion done, deleting ${v/mp4/mpeg}"
-            rm ${v/mp4/mpeg}
-        fi
+		temp=${v/mp4/mp2t}
+        mv ${v} ${temp}
+		# Check if we converted already - idempotent
+		if [[ ! "$(file --mime-type | awk '{print $2}' | cut -d'/' -f2)" == "mp4" ]]; then
+			echo "[INFO] Mime check done. Will convert."
+	    	ffmpeg -loglevel quiet -i ${temp} -c copy ${v}
+        	if [ $? -eq 0 ]; then
+        	    echo "[INFO] conversion done, deleting ${temp}"
+        	    rm ${temp}
+        	fi
+		fi
 	done
 }
 
