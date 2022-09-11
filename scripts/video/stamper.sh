530210498d61d2053cb5b5667e7d8f57c6764fa2
Rui Constancio <r.miguel.a.c@gmail.com>
2022-09-11 11:01:26 -0300

    Improve logging and break into functions

--- scripts/video/stamper.sh
+++ scripts/video/stamper.sh
@@ -3,23 +3,57 @@
 RECS_FOLDER=$1
 
 list_videos() {
-	REC_VIDS=$(find ${RECS_FOLDER} -type f -name "*.mp4" -size +17M)
+  REC_VIDS=$(find ${RECS_FOLDER} -type f -name "*.mp4" -size +17M)
 }
 
-check_if_already_collaged() {
-	for v in ${REC_VIDS}; do
-		REC_COLLAGE=${v/mp4/jpeg}
-		if [ ! -f "${REC_COLLAGE}" ]; then
-			echo "Taking collage of ${v}"
-			recordstamps.sh ${v}
-		fi
-	done
+to_log() {
+  echo "$(date +%Y%m%d%H%M%S.%N) - $1"
+}
+
+collage() {
+  for v in ${REC_VIDS}; do
+    if is_file_busy ${v}; then
+      continue
+    fi
+    if has_collage ${v}; then
+      continue 
+    fi
+    to_log "[INFO] Starting collage process for ${v}"
+    recordstamps.sh ${v}
+  done
+  to_log "[INFO] Finished collage process"
+}
+
+# Required checks to avoid taking collage of ongoing recordings or ones that already have a collage
+has_collage() {
+    to_log "[INFO] Checking if file already has collage"
+  REC_COLLAGE=${v/mp4/jpeg}
+  if [ ! -f "${REC_COLLAGE}" ]; then
+      to_log "[INFO] Collage does not exist"
+    return 1
+  fi
+
+    to_log "[INFO] Collage already exists"
+  return 0
+}
+
+# Check if file is being written to - If the file is open, we do not want to move it around as a recording is ongoing
+is_file_busy() {
+  to_log "[INFO] Checking if file is busy"
+  REC_FILE=$1
+  lsof -f -- $REC_FILE &> /dev/null
+  if [[ $? == 0 ]]; then
+    to_log "[INFO] File is busy"
+    return 0
+  fi
+
+  to_log "[INFO] File is not busy"
+  return 1
 }
 
 main() {
-	list_videos
-	check_if_already_collaged
+  list_videos
+  collage
 }
 
 main
-
--- scripts/video/tomp4converter.sh
+++ scripts/video/tomp4converter.sh
@@ -1,48 +1,70 @@
 #!/usr/bin/env bash
 
 list_videos() {
-	if [ -d $1 ]; then
-		REC_VIDS=$(find ${RECS_FOLDER} -type f -name "*.mp4" -size +17M)
-	else
-		REC_VIDS=$1	
-	fi
+  if [ -d $1 ]; then
+    REC_VIDS=$(find ${RECS_FOLDER} -type f -name "*.mp4" -size +17M)
+  else
+    REC_VIDS=$1 
+  fi
+}
+
+to_log() {
+  echo "$(date +%Y%m%d%H%M%S.%N) - $1"
 }
 
 convert() {
-	for v in ${REC_VIDS}; do
-	    echo "[INFO] Converting ${v}"
-		temp=${v/mp4/mp2t}
-		# Check if we converted already - idempotent
-		if is_file_open ${v} ; then
-			echo "[INFO] ${v} is busy. Skipping it..."
-			continue
-		fi
-		if [[ ! "$(file --mime-type ${v} | awk '{print $2}' | cut -d'/' -f2)" == "mp4" ]]; then
-        		mv ${v} ${temp}
-			echo "[INFO] Mime check done. Will convert."
-	    	ffmpeg -loglevel quiet -i ${temp} -c copy ${v}
-        	if [ $? -eq 0 ]; then
-        	    echo "[INFO] conversion done, deleting ${temp}"
-        	    rm ${temp}
-        	fi
-		fi
-	done
+  for v in ${REC_VIDS}; do
+    to_log "[INFO] Running pre-flight checks for ${v}"
+    temp=${v/mp4/mp2t}
+    # Check if file is open (writing
+    if is_file_busy ${v} ; then
+      continue
+    fi
+    # Check if we converted already - idempotent
+    if is_mime_mp4 ${v}; then
+      continue
+    fi
+  
+    # If not skipped until now, conversion should take place
+    mv ${v} ${temp}
+    ffmpeg -loglevel quiet -i ${temp} -c copy ${v}
+    if [ $? -eq 0 ]; then
+      to_log "[INFO] File converted with success, deleting ${temp}"
+      rm ${temp}
+    fi
+
+  done
 }
 
-is_file_open() {
-	# If the file is open, we do not want to move it around
-	REC_FILE=$1
-	lsof -f -- $REC_FILE &> /dev/null
-	if [[ $? == 0 ]]; then
-		return 0
-	fi
+# _Required checks_ to avoid corruption or re-doing the same conversion over and over
+# Check if mime is .mp4
+is_mime_mp4() {
+  to_log "[INFO] Checking file mime type"
+  if [[ ! "$(file --mime-type ${v} | awk '{print $2}' | cut -d'/' -f2)" == "mp4" ]]; then
+    to_log "[INFO] Mime check done. Not .mp4. Will convert"
+    return 1
+  fi
+  to_log "[INFO] Mime check done. All good, nothing to do"
+  return 0
+}
 
-	return 1
+# Check if file is being written to - If the file is open, we do not want to move it around as a recording is ongoing
+is_file_busy() {
+  to_log "[INFO] Checking if file is busy"
+  REC_FILE=$1
+  lsof -f -- $REC_FILE &> /dev/null
+  if [[ $? == 0 ]]; then
+    to_log "[INFO] File is busy"
+    return 0
+  fi
+
+  to_log "[INFO] File is not busy"
+  return 1
 }
 
 main() {
-	list_videos
-	convert
+  list_videos
+  convert
 }
 
 main
